9a7ac6a52dc4538236d079b8873a0216f2f42312
janez.baloniet@gmail.com <janez.baloniet@gmail.com>
2017-05-02 17:54:11 +0200

    RC8

--- src/app/shared/basicValidators.ts
+++ src/app/shared/basicValidators.ts
@@ -5,7 +5,7 @@ export class BasicValidators {
         // email regexp
 
         let regEx = /^[a-z0-9!#$%&'*+\/=?^_`{|}~.-]+@[a-z0-9]([a-z0-9-]*[a-z0-9])?(\.[a-z0-9]([a-z0-9-]*[a-z0-9])?)*$/i;
-        if (control.value !== '' && (control.value.length <= 5 || !regEx.test(control.value))) {
+        if (control && control.value && control.value !== '' && (control.value.length <= 5 || !regEx.test(control.value))) {
             return { 'email': true };
         }
         /*
